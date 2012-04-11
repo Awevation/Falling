230c5d457ab758035f8a4c82285227c5124b0f22
Milo Mordaunt <milomord@gmail.com>
2012-04-11 19:51:28 +0100

    deleted:    images/cloud.png-bak 	new file:   js/BoundingBox.js
    physics now suck less

---
+++ site/js/BoundingBox.js
@@ -0,0 +1,8 @@
+function BoundingBox(xOff, yOff, width, height) {
+    this.xOff = xOff;
+    this.yOff = yOff;
+    this.xPos = 0;
+    this.yPos = 0;
+    this.width = width;
+    this.height = height;
+}
